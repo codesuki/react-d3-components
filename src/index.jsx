ba2e04b59231ad289e120b28dc2110cd89d18716
Jacob Boon <jacob@wearelandscape.nl>
2016-07-27 17:43:23 +0200

    export all components

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,32 @@
-let BarChart = require('./BarChart');
-let Waveform = require('./Waveform');
-let PieChart = require('./PieChart');
-let ScatterPlot = require('./ScatterPlot');
-let LineChart = require('./LineChart');
-let AreaChart = require('./AreaChart');
-let Brush = require('./Brush');
-let d3 = require('d3');
+let components = {};
 
-module.exports = {
-    BarChart: BarChart,
-    PieChart: PieChart,
-    ScatterPlot: ScatterPlot,
-    LineChart: LineChart,
-    AreaChart: AreaChart,
-    Waveform: Waveform,
-    Brush: Brush,
-    d3: d3
-};
+// d3 itself
+components.d3 = require('d3');
+
+// Charts
+components.AreaChart = require('./AreaChart');
+components.BarChart = require('./BarChart');
+components.LineChart = require('./LineChart');
+components.PieChart = require('./PieChart');
+components.ScatterPlot = require('./ScatterPlot');
+components.Waveform = require('./Waveform');
+
+// Components
+components.Brush = require('./Brush');
+components.Axis = require('./Axis');
+components.Bar = require('./Bar');
+components.Chart = require('./Chart');
+components.Path = require('./Path');
+components.Tooltip = require('./Tooltip');
+
+// Mixins
+components.AccessorMixin = require('./AccessorMixin');
+components.ArrayifyMixin = require('./ArrayifyMixin');
+components.DefaultPropsMixin = require('./DefaultPropsMixin');
+components.DefaultScalesMixin = require('./DefaultScalesMixin');
+components.HeightWidthMixin = require('./HeightWidthMixin');
+components.StackAccessorMixin = require('./StackAccessorMixin');
+components.StackDataMixin = require('./StackDataMixin');
+components.TooltipMixin = require('./TooltipMixin');
+
+module.exports = components;
