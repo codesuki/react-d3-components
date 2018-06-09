25a8e3e197492adced42a7a5ca34cf462d436882
David Narbutovich <vamnemne@gmail.com>
2018-06-09 19:54:37 +0300

    Update webpack configs

---
+++ config/webpack.config.base.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/index.jsx'),
+    output: {
+        library: 'ReactD3',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, '../dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, '../src')
+            }
+        ]
+    },
+    externals: {
+        d3: true,
+        react: 'React',
+        'react-dom': 'ReactDOM'
+    }
+};
